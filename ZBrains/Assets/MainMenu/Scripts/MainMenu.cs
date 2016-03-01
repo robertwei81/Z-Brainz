bfbbdfedc896b46d880d37c575a523fd0a64e9b6
nancytorres8094 <nancytorres8094@gmail.com>
2016-02-29 21:12:15 -0800

    Added ZBrains Project
    As of right now it only has a simple main menu. The buttons have not
    been added. I felt like I needed to start the main menu so that I can
    add the help button.

---
+++ ZBrains/Assets/MainMenu/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿/// <summary>
+/// Main menu.
+/// Attached to Main Camera
+/// </summary>
+
+using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public Texture backgroundTexture; // public so you can drag background image on unity
+
+	void OnGUI(){
+		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), backgroundTexture);
+	}
+		
+}
