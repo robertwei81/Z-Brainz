e8e5672991c353d648a7e4111424794dd8a4bb17
nancytorres8094 <nancytorres8094@gmail.com>
2016-03-01 17:03:30 -0800

    Tilt Input
    Added the CS file with changes to accept tilt input.

---
+++ PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody2D rb2d;
+	public float speed;
+	public Text countText;
+	public Text winText;
+	private int count;
+
+	void Start()
+	{
+		Screen.orientation = ScreenOrientation.LandscapeLeft;
+		//Screen.orientation = ScreenOrientation.LandscapeLeft;
+		rb2d = GetComponent<Rigidbody2D> ();
+		count = 0;
+		winText.text = "";
+		SetCountText ();
+	}
+
+    void FixedUpdate()
+    {
+		float moveHorizontal = Input.acceleration.x;//GetAxis("Horizontal");
+		float moveVertical = Input.acceleration.y;//.GetAxis ("Vertical");
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb2d.AddForce (movement*speed);
+    }
+
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		if (other.gameObject.CompareTag ("PickUp")) 
+		{
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) 
+		{
+			winText.text = "You win!";
+		}
+			
+
+	}
+}
