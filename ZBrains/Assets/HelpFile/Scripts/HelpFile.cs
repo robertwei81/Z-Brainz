56c1d930fc8e1468b1934c761e0f554703e215de
nancytorres8094 <nancytorres8094@gmail.com>
2016-03-01 23:35:30 -0800

    Help File
    Added the help button. Also changed the code so that it opens the help
    file template when the help button is clicked.

---
+++ ZBrains/Assets/HelpFile/Scripts/HelpFile.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelpFile : MonoBehaviour {
+
+	public Texture backgroundTexture; // public so you can drag background image on unity
+
+	void OnGUI(){
+		// display background texture
+		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), backgroundTexture);
+	}
+	  
+
+}
--- ZBrains/Assets/MainMenu/Scripts/MainMenu.cs
+++ ZBrains/Assets/MainMenu/Scripts/MainMenu.cs
@@ -5,13 +5,24 @@
 
 using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
 	public Texture backgroundTexture; // public so you can drag background image on unity
 
 	void OnGUI(){
+		// display background texture
 		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), backgroundTexture);
+		if (GUI.Button (new Rect (Screen.width * .35f, Screen.height * .9f, Screen.width * .3f, Screen.height * .1f), "Help")) 
+		{
+			print("Clicked!"); // testing purposes
+			SceneManager.LoadScene("Help File");  // change scene based on name
+		}
+
+
+
 	}
+
 		
 }
