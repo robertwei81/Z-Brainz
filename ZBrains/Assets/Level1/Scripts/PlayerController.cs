b398e9569cfc2f3c89968511eac90c6f9cf04692
nancytorres8094 <nancytorres8094@gmail.com>
2016-03-08 01:47:44 -0800

    Added Player Movement
    Added the zombie sprite and added a temporary floor. The zombie can be
    moved using tilt.

---
+++ ZBrains/Assets/Level1/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody2D rb2d;
+	public float speed;
+
+	void Start()
+	{
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+
+
+	void FixedUpdate () 
+	{
+		float moveHorizontal = Input.acceleration.x;
+		float moveVertical = Input.acceleration.y;
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb2d.AddForce (movement * speed);
+	}
+}
