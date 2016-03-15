1ae148ff0fc1a593bd2a9ce7b17745c3564ca808
Christian Whittington <christiangwhittington@gmail.com>
2016-03-14 20:34:55 -0700

    Exit Game Button
    Main Menu now has an exit game button. I also adjusted height of all
    buttons for a cleaner looking interface.

--- ZBrains/Assets/MainMenu/Scripts/MainMenu.cs
+++ ZBrains/Assets/MainMenu/Scripts/MainMenu.cs
@@ -20,10 +20,17 @@ public class MainMenu : MonoBehaviour {
 			SceneManager.LoadScene("HelpFile");  // change scene based on name
 		}
 		// Create "Start Game" button
-		if (GUI.Button (new Rect (Screen.width * .35f, Screen.height * .6f, Screen.width * .3f, Screen.height * .1f), "Start Game")) 
+		if (GUI.Button (new Rect (Screen.width * .35f, Screen.height * .65f, Screen.width * .3f, Screen.height * .1f), "Start Game")) 
 		{
 			// Change scene to "Level1" when this button is clicked.
 			SceneManager.LoadScene("Level1");
+		}
+
+		// Create "Exit Game" button
+		if (GUI.Button (new Rect (Screen.width * .35f, Screen.height * .75f, Screen.width * .3f, Screen.height * .1f), "Exit Game")) 
+		{
+			// Exit application when button is clicked.
+			Application.Quit();
 		}
 	}
 
