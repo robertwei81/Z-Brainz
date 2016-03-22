b0ba75e550c83c2fdf7384b942af9057b5e152f3
Christian Whittington <christiangwhittington@gmail.com>
2016-03-22 15:33:38 -0700

    Touch to Pause
    If screen is double tapped, should pause game as long as taps are
    within same Update time frame. No label yet. Double tap while paused
    should continue game.

--- ZBrains/Assets/Level1/Scripts/PlayerController.cs
+++ ZBrains/Assets/Level1/Scripts/PlayerController.cs
@@ -6,12 +6,29 @@ public class PlayerController : MonoBehaviour {
 
 	private Rigidbody2D rb2d;
 	public float speed;
-
+	public bool paused = false;
 	void Start()
 	{
 		rb2d = GetComponent<Rigidbody2D> ();
 	}
-
+	// Double touch screen to pause the game
+	void Update(){
+		// If screen is tapped more than once in a frame
+		if (Input.touchCount > 1) {
+			// Check if game is paused already
+			if (paused == false) {
+				// set paused flag 
+				paused = true;
+				// pause game
+				Time.timeScale = 0;
+			} else {
+				// reset paused flag
+				paused = false;
+				// continue game
+				Time.timeScale = 1;
+			}
+		}
+	}
 
 	void FixedUpdate () 
 	{
