ae0471241235f04aab782442534b8e4e1b200474
nancytorres8094 <nancytorres8094@gmail.com>
2016-03-23 19:26:13 -0700

    Added functionality to Door
    The door now opens when the player gets near it. It also leads to the
    "next level" (Menu).

--- ZBrains/Assets/Level1/Scripts/PlayerController.cs
+++ ZBrains/Assets/Level1/Scripts/PlayerController.cs
@@ -7,15 +7,17 @@ public class PlayerController : MonoBehaviour {
 	private Rigidbody2D rb2d;
 	public float speed;
 	public bool paused = false;
+	public Sprite openedDoor;
+
 	void Start()
 	{
 		rb2d = GetComponent<Rigidbody2D> ();
-	}
+	} // end Start
 
 	void FixedUpdate () 
 	{
 		// If screen is tapped more than once in a frame
-		if (Input.touches[0].tapCount == 2) {
+		/*if (Input.touches[0].tapCount == 2) {
 			// Check if game is paused already
 			if (paused == false) {
 				// set paused flag 
@@ -28,7 +30,7 @@ public class PlayerController : MonoBehaviour {
 				// continue game
 				Time.timeScale = 1;
 			}
-		}
+		}*/
 		float moveHorizontal = Input.acceleration.x;
 		float moveVertical = Input.acceleration.y;
 		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
@@ -37,6 +39,15 @@ public class PlayerController : MonoBehaviour {
 		{
 			SceneManager.LoadScene("MainMenu");
 		}
+	} // end FixedUpdate
 
-	}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag ("Door")) 
+		{
+			other.gameObject.GetComponent<SpriteRenderer> ().sprite = openedDoor;
+			SceneManager.LoadScene("MainMenu");
+		}
+
+	} //end OnTriggerEnter2D
 }
