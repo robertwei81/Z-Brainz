63e8b6f452aafe6e76c73e75b79ece25ec82f3a9
nancytorres8094 <nancytorres8094@gmail.com>
2016-04-07 16:11:36 -0700

    Added Built Game and Variable
    I added the missing variable and added the built game with updated
    changes.

--- ZBrains/Assets/Scripts/PlayerController.cs
+++ ZBrains/Assets/Scripts/PlayerController.cs
@@ -13,6 +13,7 @@ public class PlayerController : MonoBehaviour {
 	public Text pauseText;
 	public Text countText;
 	private int starting_count;
+	public int difficultyTimeVariable;
 	void Start()
 	{
 		rb2d = GetComponent<Rigidbody2D> ();
