ac4796feb57dd2e5a6cf11d79872ef34298d5d62
robertwei81 <robertwei@yahoo.com>
2016-04-18 13:15:54 -0700

    HelpFile Script Fix-and-tested
    DONE :-D

--- ZBrains/Assets/Scripts/HelpFile.cs
+++ ZBrains/Assets/Scripts/HelpFile.cs
@@ -94,34 +94,9 @@ public class HelpFile : MonoBehaviour {
 	void SlideLeft(){
 		if (index < 3)
 			index = index + 1;
-		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), HelpSlides[index]);
 	}
 	void SlideRight(){
 		if (index > 0)
 			index = index - 1;
-		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), HelpSlides[index]);
 	}
 }
-
-/*
-using UnityEngine;
-using System.Collections;
-using UnityEngine.SceneManagement;
-
-public class HelpFile : MonoBehaviour {
-
-	public Texture backgroundTexture; // public so you can drag background image on unity
-
-	void OnGUI(){
-		// display background texture
-		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), backgroundTexture);
-	}
-
-	void FixedUpdate(){
-		if (Input.GetKeyDown (KeyCode.Escape)) // on return go to Main Menu
-		{
-			SceneManager.LoadScene("MainMenu");
-		}
-	}
-}
-*/
