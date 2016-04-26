f4605421a957b7f69ca337ac30ca659a73d2aa1c
Christian Whittington <christiangwhittington@gmail.com>
2016-04-25 19:55:58 -0700

    Update blood sorting layer
    I’ve updated bloods sorting layer to be Background instead of Brains.

--- ZBrains/Assets/Scripts/PlayerController.cs
+++ ZBrains/Assets/Scripts/PlayerController.cs
@@ -140,6 +140,7 @@ public class PlayerController : MonoBehaviour {
 			//other.gameObject.SetActive (false);
 			other.gameObject.GetComponent<SpriteRenderer> ().sprite = bloodPool;
 			other.gameObject.tag = "Blood";
+			other.gameObject.GetComponent<SpriteRenderer> ().sortingLayerName = "Background";
 			count = count + 1;
 			setCountText ();
 		}
